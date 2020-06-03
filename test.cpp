a4326c460646bf0d9ec7962f75a3348292d55f18
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-06-03 17:59:31 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int mxN = 1e4;
+char s[mxN];
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin.get(s, mxN, '\n');
+	cout << s << "\n";
+	return 0;
+}
